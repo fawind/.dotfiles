ad5e9d6b52d23e41a5ad1daf929e092d07f555f0
fawind <windheuser.fabian@gmail.com>
2015-04-16 15:34:23 +0200

    Add screenlayout defaults

---
+++ screenlayout/home.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI2 --off --output HDMI1 --off --output LVDS1 --off --output VGA1 --mode 1920x1080 --pos 1920x0 --rotate normal
+xset m 5/2 0
