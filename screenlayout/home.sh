45091c0d7a15372af1d54c16117bf2b063225c1e
fawind <windheuser.fabian@gmail.com>
2015-05-13 09:07:30 +0200

    Change monitor source

--- screenlayout/home.sh
+++ screenlayout/home.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI2 --off --output HDMI1 --off --output LVDS1 --off --output VGA1 --mode 1920x1080 --pos 1920x0 --rotate normal
+xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --off --output LVDS1 --off --output VGA1 --off
 xset m 5/2 0
